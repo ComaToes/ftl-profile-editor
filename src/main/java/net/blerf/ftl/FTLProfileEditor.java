9279ccbc4397a39f5d8581840af57f9d4d5b5301
ComaToes <comatoes@blerf.net>
2012-09-29 15:00:26 +0100

    Release v9

--- src/main/java/net/blerf/ftl/FTLProfileEditor.java
+++ src/main/java/net/blerf/ftl/FTLProfileEditor.java
@@ -10,7 +10,7 @@ public class FTLProfileEditor {
 
 	private static final Logger log = LogManager.getLogger(FTLProfileEditor.class);
 	
-	private static final int VERSION = 8;
+	private static final int VERSION = 9;
 
 	public static void main(String[] args) {
 
