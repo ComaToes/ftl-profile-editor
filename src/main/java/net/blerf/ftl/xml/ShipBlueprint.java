819b4e63d919b894430b12402611f887241b971c
ComaToes <comatoes@blerf.net>
2012-10-01 04:53:02 +0100

    Expanded parser model for ship systems list

--- src/main/java/net/blerf/ftl/xml/ShipBlueprint.java
+++ src/main/java/net/blerf/ftl/xml/ShipBlueprint.java
@@ -1,5 +1,8 @@
 package net.blerf.ftl.xml;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
@@ -21,7 +24,7 @@ public class ShipBlueprint {
 	private String shipClass;
 	private String name, desc;
 	
-	private Object systemList; // TODO model
+	private SystemList systemList;
 	
 	private int weaponSlots, droneSlots;
 	
@@ -29,7 +32,188 @@ public class ShipBlueprint {
 	
 	@XmlRootElement
 	@XmlAccessorType(XmlAccessType.FIELD)
-	public class SystemList {
+	public static class SystemList {
+
+		@XmlRootElement
+		@XmlAccessorType(XmlAccessType.FIELD)
+		public static class RoomSlot {
+			@XmlElement(required=false)
+			private String direction;
+			@XmlElement
+			private int number;
+			
+			public String getDirection() {
+				return direction;
+			}
+			public void setDirection(String direction) {
+				this.direction = direction;
+			}
+			public int getNumber() {
+				return number;
+			}
+			public void setNumber(int number) {
+				this.number = number;
+			}
+			
+		}
+		
+		@XmlRootElement
+		@XmlAccessorType(XmlAccessType.FIELD)
+		public static class SystemRoom {
+			
+			@XmlAttribute
+			private int power;
+			@XmlAttribute(name="room")
+			private int roomId;
+			@XmlAttribute(required=false)
+			private boolean start;
+			@XmlAttribute(required=false)
+			private String img;
+			@XmlElement(required=false)
+			private RoomSlot slot;
+			
+			public int getPower() {
+				return power;
+			}
+			public void setPower(int power) {
+				this.power = power;
+			}
+			public int getRoomId() {
+				return roomId;
+			}
+			public void setRoomId(int roomId) {
+				this.roomId = roomId;
+			}
+			public boolean isStart() {
+				return start;
+			}
+			public void setStart(boolean start) {
+				this.start = start;
+			}
+			public String getImg() {
+				return img;
+			}
+			public void setImg(String img) {
+				this.img = img;
+			}
+			public RoomSlot getSlot() {
+				return slot;
+			}
+			public void setSlot(RoomSlot slot) {
+				this.slot = slot;
+			}
+			
+		}
+		
+		@XmlElement(name="pilot")
+		private SystemRoom pilotRoom;
+		@XmlElement(name="doors")
+		private SystemRoom doorsRoom;
+		@XmlElement(name="sensors")
+		private SystemRoom sensorsRoom;
+		@XmlElement(name="medbay")
+		private SystemRoom medicalRoom;
+		@XmlElement(name="oxygen")
+		private SystemRoom lifeSupportRoom;
+		@XmlElement(name="shields")
+		private SystemRoom shieldRoom;
+		@XmlElement(name="engines")
+		private SystemRoom engineRoom;
+		@XmlElement(name="weapons")
+		private SystemRoom weaponRoom;
+		@XmlElement(name="drones")
+		private SystemRoom droneRoom;
+		@XmlElement(name="teleporter")
+		private SystemRoom teleporterRoom;
+		@XmlElement(name="cloaking")
+		private SystemRoom cloakRoom; // lol :)
+		@XmlElement(name="artillery",required=false)
+		private List<SystemRoom> artilleryRooms;
+		
+		public SystemRoom[] getSystemRooms() {
+			SystemRoom[] rooms = new SystemRoom[] { pilotRoom, doorsRoom, sensorsRoom, medicalRoom, lifeSupportRoom, shieldRoom, 
+					engineRoom, weaponRoom, droneRoom, teleporterRoom, cloakRoom };
+			List<SystemRoom> list = new ArrayList<SystemRoom>();
+			for (SystemRoom room : rooms) {
+				list.add(room);
+			}
+			if( artilleryRooms != null )
+				list.addAll(artilleryRooms);
+			return list.toArray(new SystemRoom[list.size()]);
+		}
+		
+		public SystemRoom getPilotRoom() {
+			return pilotRoom;
+		}
+		public void setPilotRoom(SystemRoom pilotRoom) {
+			this.pilotRoom = pilotRoom;
+		}
+		public SystemRoom getDoorsRoom() {
+			return doorsRoom;
+		}
+		public void setDoorsRoom(SystemRoom doorsRoom) {
+			this.doorsRoom = doorsRoom;
+		}
+		public SystemRoom getSensorsRoom() {
+			return sensorsRoom;
+		}
+		public void setSensorsRoom(SystemRoom sensorsRoom) {
+			this.sensorsRoom = sensorsRoom;
+		}
+		public SystemRoom getMedicalRoom() {
+			return medicalRoom;
+		}
+		public void setMedicalRoom(SystemRoom medicalRoom) {
+			this.medicalRoom = medicalRoom;
+		}
+		public SystemRoom getLifeSupportRoom() {
+			return lifeSupportRoom;
+		}
+		public void setLifeSupportRoom(SystemRoom lifeSupportRoom) {
+			this.lifeSupportRoom = lifeSupportRoom;
+		}
+		public SystemRoom getShieldRoom() {
+			return shieldRoom;
+		}
+		public void setShieldRoom(SystemRoom shieldRoom) {
+			this.shieldRoom = shieldRoom;
+		}
+		public SystemRoom getEngineRoom() {
+			return engineRoom;
+		}
+		public void setEngineRoom(SystemRoom engineRoom) {
+			this.engineRoom = engineRoom;
+		}
+		public SystemRoom getWeaponRoom() {
+			return weaponRoom;
+		}
+		public void setWeaponRoom(SystemRoom weaponRoom) {
+			this.weaponRoom = weaponRoom;
+		}
+		public SystemRoom getDroneRoom() {
+			return droneRoom;
+		}
+		public void setDroneRoom(SystemRoom droneRoom) {
+			this.droneRoom = droneRoom;
+		}
+		public SystemRoom getTeleporterRoom() {
+			return teleporterRoom;
+		}
+		public void setTeleporterRoom(SystemRoom teleporterRoom) {
+			this.teleporterRoom = teleporterRoom;
+		}
+		public SystemRoom getCloakRoom() {
+			return cloakRoom;
+		}
+		public void setCloakRoom(SystemRoom cloakRoom) {
+			this.cloakRoom = cloakRoom;
+		}
+		public List<SystemRoom> getArtilleryRooms() {
+			return artilleryRooms;
+		}
+		public void setArtilleryRooms(List<SystemRoom> artilleryRooms) {
+			this.artilleryRooms = artilleryRooms;
+		}
 		
 	}
 
@@ -81,11 +265,11 @@ public class ShipBlueprint {
 		this.desc = desc;
 	}
 
-	public Object getSystemList() {
+	public SystemList getSystemList() {
 		return systemList;
 	}
 
-	public void setSystemList(Object systemList) {
+	public void setSystemList(SystemList systemList) {
 		this.systemList = systemList;
 	}
 
