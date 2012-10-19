ComaToes/ftl-profile-editor

commit 34b470431327ae666de99f57de30f2af0fe86c23

Fixed broken var names in commit d70b470

src/main/java/net/blerf/ftl/ui/FTLFrame.java
@@ -410,13 +410,13 @@ public class FTLFrame extends JFrame {
 						log.trace("Read completed successfully");
 						
 					} catch( Exception e ) {
-						log.error( "Error reading profile", e );
-						showErrorDialog( "Error reading profile: " + e.getMessage() );
+						log.error( "Error reading profile", f );
+						showErrorDialog( "Error reading profile: " + f.getMessage() );
 					} finally {
 						try {if (raf != null) raf.close();}
-						catch (IOException f) {}
+						catch (IOException g) {}
 						try {if (in != null) in.close();}
-						catch (IOException f) {}
+						catch (IOException g) {}
 					}
 				} else {
 					log.trace("Open dialog cancelled");

commit 78141ce229ac3a1b3c7a099bb67ec734a65c085e

Fixed broken var name in commit 34b4704

src/main/java/net/blerf/ftl/ui/FTLFrame.java
@@ -409,7 +409,7 @@ public class FTLFrame extends JFrame {
 						
 						log.trace("Read completed successfully");
 						
-					} catch( Exception e ) {
+					} catch( Exception f ) {
 						log.error( "Error reading profile", f );
 						showErrorDialog( "Error reading profile: " + f.getMessage() );
 					} finally {