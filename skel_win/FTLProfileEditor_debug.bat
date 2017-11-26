6919fd1c88720fef0363e5ec5ef073d00fef7e50
Vhati <tvtronix@yahoo.com>
2017-11-26 14:07:43 -0500

    Added 'FTLProfileEditor_debug.bat' for troubleshooting on Windows

---
+++ skel_win/FTLProfileEditor_debug.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+SETLOCAL
+SET L4J_LOG_NAME=launch4j.log
+SET EDITOR_LOG_NAME=profile-editor-log.txt
+SET L4J_LOG_PATH=%~dp0launch4j.log
+SET EDITOR_LOG_PATH=%~dp0profile-editor-log.txt
+
+IF EXIST "%L4J_LOG_PATH%" DEL /Q "%L4J_LOG_PATH%"
+IF EXIST "%EDITOR_LOG_PATH%" DEL /Q "%EDITOR_LOG_PATH%"
+
+ECHO.
+ECHO.
+ECHO This script will try to run the Profile/SavedGame Editor.
+ECHO.
+ECHO When it exits, logs will be presented in multiple notepad windows.
+ECHO.
+ECHO.
+PAUSE
+ECHO.
+
+ECHO Running the Editor...
+ECHO.
+"%~dp0FTLProfileEditor.exe" --l4j-debug
+
+ECHO Spawning notepad...
+ECHO.
+IF EXIST "%L4J_LOG_PATH%" (
+  ECHO For Java issues, see "%L4J_LOG_NAME%"
+  ECHO.
+  START /B notepad.exe "%L4J_LOG_PATH%"
+) ELSE (
+  ECHO Missing log: "%L4J_LOG_NAME%"
+)
+
+IF EXIST "%EDITOR_LOG_PATH%" (
+  ECHO For Editor issues, see "%EDITOR_LOG_NAME%"
+  ECHO.
+  START /B notepad.exe "%EDITOR_LOG_PATH%"
+) ELSE (
+  ECHO Missing log: "%EDITOR_LOG_NAME%"
+)
+
+ECHO.
+ECHO.
+ECHO Interesting logs can be pasted on the FTL forum between [code] [/code] tags.
+ECHO.
+ECHO.
+ECHO This script will now exit.
+ECHO.
+PAUSE
+ENDLOCAL & EXIT /B
